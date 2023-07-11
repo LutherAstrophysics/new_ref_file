284547e9a8c1264740c8f4e4df6aa2b12dd37dd9
suman chapai <sumanchapai@gmail.com>
2023-07-11 10:43:14 -0500

    Remove hot pixels

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ from astropy.stats import sigma_clipped_stats
 from astropy.io.fits import getdata
 from photutils.detection import IRAFStarFinder, DAOStarFinder
 from pathlib import Path
+from astroalign import find_transform
+from ccdproc import cosmicray_lacosmic as lacosmic
 
 
 def main(img, daofind=False):
     data = getdata(img)  # 2003-8-3
+
+    # Remove hot pixels
+    data, _ = lacosmic(data)
+    data = np.array(data)
+
     mean, median, std = sigma_clipped_stats(data, sigma=3.0)
+
     print((mean, median, std))
 
     # Mask out extreme edges
@@ -21,12 +29,13 @@ def main(img, daofind=False):
     mask[0:12, :] = True
     mask[1012:,] = True
 
-    no_of_sigmas_above_bg = 6
+    no_of_sigmas_above_bg = 5
+    fwhm = 3.0
     if daofind:
-        daofind = DAOStarFinder(fwhm=3.0, threshold=no_of_sigmas_above_bg * std)
+        daofind = DAOStarFinder(fwhm=fwhm, threshold=no_of_sigmas_above_bg * std)
         sources = daofind(data - median, mask=mask)
     else:
-        irafind = IRAFStarFinder(fwhm=3.0, threshold=no_of_sigmas_above_bg * std)
+        irafind = IRAFStarFinder(fwhm=fwhm, threshold=no_of_sigmas_above_bg * std)
         sources = irafind(data - median, mask=mask)
 
     for col in sources.colnames:
@@ -42,10 +51,20 @@ def main(img, daofind=False):
     sources.write(name, format="ascii", overwrite=True)
 
     positions = np.transpose((sources["xcentroid"], sources["ycentroid"]))
+    xs, ys = sources["xcentroid"], sources["ycentroid"]
+    print("Median X, Y", np.median(xs), np.median(ys))
+
     apertures = CircularAperture(positions, r=4.0)
     norm = ImageNormalize(stretch=SqrtStretch())
     plt.imshow(data, cmap="Greys", origin="lower", norm=norm, interpolation="nearest")
-    stars = [(541.2, 475.65), (547.57, 493.95), (746.58, 459.64), (539.54, 426.1)]
+
+    # align the image to our reference
+    t = find_transform(getdata("reference/m23_3.5_071.fit"), data, detection_sigma=10, min_area=10)[0]
+
+    # stars = [(541.2, 475.65), (547.57, 493.95), (746.58, 459.64), (539.54, 426.1)]
+    stars = t([(541.2, 475.65), (547.57, 493.95), (746.58, 459.64), (539.54, 426.1)])
+    # breakpoint()
+
     stars_x, stars_y = zip(*stars)
     plt.plot(stars_x, stars_y, "r.", markersize=4)
     apertures.plot(color="blue", lw=1.5, alpha=0.5)
@@ -54,5 +73,4 @@ def main(img, daofind=False):
 
 
 if __name__ == "__main__":
-    # main("reference/m23_3.5_071.fit", daofind=True)
     main("m23_7.0_2022-06-19_0028.fit", daofind=True)
